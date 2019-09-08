26ad240a99cde6e1bcfb062cf8af3ee99dcf9b14
dbystruev <github@learnswift.ru>
2019-09-08 20:56:44 +0300

    Print XML structure and example data

---
+++ XML Parser/App/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  XML Parser
+//
+//  Created by Denis Bystruev on 08/09/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
